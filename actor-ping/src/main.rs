32da46053f4e1492e543ba29ff89e562f0983446
ittokun <73953416+ittokun@users.noreply.github.com>
2023-02-18 04:02:22 +0900

    Finished actix/actor

--- actor-ping/src/main.rs
+++ actor-ping/src/main.rs
@@ -1,38 +1,81 @@
+use actix::dev::{MessageResponse, OneshotSender};
 use actix::prelude::*;
 
-struct MyActor {
-    count: usize,
+#[derive(Message)]
+#[rtype(result = "Responses")]
+enum Messages {
+    Ping,
+    Pong,
 }
 
+enum Responses {
+    GotPing,
+    GotPong,
+}
+
+impl<A, M> MessageResponse<A, M> for Responses
+where
+    A: Actor,
+    M: Message<Result = Responses>,
+{
+    fn handle(self, _ctx: &mut A::Context, tx: Option<OneshotSender<M::Result>>) {
+        if let Some(tx) = tx {
+            tx.send(self);
+        }
+    }
+}
+
+/// Define actor
+struct MyActor;
+
+/// Provide Actor implementation for our actor
 impl Actor for MyActor {
     type Context = Context<Self>;
+
+    fn started(&mut self, _ctx: &mut Self::Context) {
+        println!("Actor is alive");
+    }
+
+    fn stopped(&mut self, _ctx: &mut Self::Context) {
+        println!("Actor is stopped");
+    }
 }
 
-#[derive(Message)]
-#[rtype(result = "usize")]
-struct Ping(usize);
+/// Define handler for `Messages` enum
+impl Handler<Messages> for MyActor {
+    type Result = Responses;
 
-impl Handler<Ping> for MyActor {
-    type Result = usize;
-
-    fn handle(&mut self, msg: Ping, _ctx: &mut Self::Context) -> Self::Result {
-        self.count += msg.0;
-
-        self.count
+    fn handle(&mut self, msg: Messages, _ctx: &mut Self::Context) -> Self::Result {
+        match msg {
+            Messages::Ping => Responses::GotPing,
+            Messages::Pong => Responses::GotPong,
+        }
     }
 }
 
 #[actix_rt::main]
 async fn main() {
-    // start new actor
-    let addr = MyActor { count: 10 }.start();
+    // Start MyActor in current thread
+    let addr = MyActor.start();
 
-    // send message and get future for result
-    let res = addr.send(Ping(10)).await;
+    // Send Ping message.
+    // send() message returns Future object, that resolves to message result
+    let ping_future = addr.send(Messages::Ping).await;
+    let pong_future = addr.send(Messages::Pong).await;
 
-    // handle() returns tokio handle
-    println!("RESULT: {}", res.unwrap() == 20);
+    match pong_future {
+        Ok(res) => match res {
+            Responses::GotPing => println!("Ping received"),
+            Responses::GotPong => println!("Pong received"),
+        },
+        Err(e) => println!("Actor is probably dead: {}", e),
+    }
 
-    // stop system and exit
-    System::current().stop();
+    match ping_future {
+        Ok(res) => match res {
+            Responses::GotPing => println!("Ping received"),
+            Responses::GotPong => println!("Pong received"),
+        },
+        Err(e) => println!("Actor is probably dead: {}", e),
+    }
 }
