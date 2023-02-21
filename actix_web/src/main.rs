5932e06c4df53c66746aa038c7762d55eb9aaa47
ittokun <73953416+ittokun@users.noreply.github.com>
2023-02-21 20:10:44 +0900

    actix-web: writing an Application

--- actix_web/src/main.rs
+++ actix_web/src/main.rs
@@ -1,8 +1,44 @@
-use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder, guard};
+use std::sync::Mutex;
+
+struct AppStateWithCounter {
+    app_name: String,
+    counter: Mutex<i32>,
+}
+
+fn scoped_config(cfg: &mut web::ServiceConfig) {
+    cfg.service(
+        web::resource("/test")
+        .route(web::get().to(|| async { HttpResponse::Ok().body("test") }))
+            .route(web::head().to(HttpResponse::MethodNotAllowed)),
+    );
+}
+
+fn config(cfg: &mut web::ServiceConfig) {
+    cfg.service(
+        web::resource("/app")
+            .route(web::get().to(|| async { HttpResponse::Ok().body("app") }))
+            .route(web::head().to(HttpResponse::MethodNotAllowed))
+    );
+}
 
 #[get("/")]
+async fn index(data: web::Data<AppStateWithCounter>) -> String {
+    let app_name = &data.app_name;
+    let mut counter = data.counter.lock().unwrap();
+    *counter += 1;
+
+    format!("Hello {app_name}, Request number: {counter}")
+}
+
+#[get("/hello")]
 async fn hello() -> impl Responder {
     HttpResponse::Ok().body("Hello world!")
+}
+
+#[get("/show")]
+async fn show_users() -> impl Responder {
+    HttpResponse::Ok().body("Alice, Bob, Chris, Dan, Eve")
 }
 
 #[post("/echo")]
@@ -14,13 +50,43 @@ async fn manual_hello() -> impl Responder {
     HttpResponse::Ok().body("Hey there!")
 }
 
+async fn app() -> impl Responder {
+    "Hello world!"
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
+    let counter = web::Data::new(AppStateWithCounter {
+        app_name: String::from("Actix Web"),
+        counter: Mutex::new(0),
+    });
+
+    HttpServer::new(move || {
         App::new()
+            .configure(config)
+            .app_data(counter.clone())
+            .service(web::scope("/api").configure(scoped_config))
+            .service(
+                web::scope("/")
+                    .guard(guard::Header("Host", "www.rust-lang.org"))
+                    .route("", web::to(|| async { HttpResponse::Ok().body("www") })),
+            )
+            .service(
+                web::scope("/")
+                    .guard(guard::Header("Host", "users.rust-lang.org"))
+                    .route("", web::to(|| async { HttpResponse::Ok().body("user") })),
+            )
+            .service(index)
             .service(hello)
             .service(echo)
             .route("/hey", web::get().to(manual_hello))
+            .service(
+                web::scope("/users").service(show_users)
+            )
+            .service(
+                web::scope("/app")
+                    .route("/index.html", web::get().to(app)),
+            )
     })
     .bind(("127.0.0.1", 8080))?
     .run()
